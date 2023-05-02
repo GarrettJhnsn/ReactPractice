5d878bea289b8c30d79a7ccaf9ed873f9fe84068
Garrett Johnson <Garrett.Johnson@ey.com>
2023-05-02 10:37:56 -0700

    Validation To Use Reusable Custom Input Hook

---
+++ Forms/src/hooks/use-input.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  //Checks each keystroke
+  const valueChangeHandler = (e) => {
+    setEnteredValue(e.target.value);
+    console.log(e.target.value);
+  };
+
+  //Click Inside Input Box and Outside W/ Out Input Displays Error
+  const inputBlurHandler = (e) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+export default useInput;
