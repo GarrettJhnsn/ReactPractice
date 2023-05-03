96c8bf9c4fad01bd0d824e7cf74fb8a96048debd
Garrett Johnson <Garrett.Johnson@ey.com>
2023-05-03 16:09:20 -0700

    added http request for meals

--- FoodOrderingApp/src/components/Meals/AvailableMeals.js
+++ FoodOrderingApp/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,37 @@
+import { useEffect, useState } from "react";
 import style from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American cheese, grass fed beef",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green!",
-    price: 18.99,
-  },
-];
+import PizzaSpinner from "./PizzaSpinner";
 
 export default function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const res = await fetch(
+        "https://movielist-0808-default-rtdb.firebaseio.com/meals.json"
+      );
+      const resData = await res.json();
+      const loadedMeals = [];
+
+      for (const key in resData) {
+        loadedMeals.push({
+          id: key,
+          name: resData[key].name,
+          description: resData[key].description,
+          price: resData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    setTimeout(fetchMeals, 2500);
+  }, [isLoading]);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -40,6 +41,13 @@ export default function AvailableMeals() {
     />
   ));
 
+  if (isLoading) {
+    return (
+      <section className={style.meals}>
+        <PizzaSpinner />
+      </section>
+    );
+  }
   return (
     <section className={style.meals}>
       <Card>
--- FoodOrderingApp/src/components/Meals/MealsSummary.js
+++ FoodOrderingApp/src/components/Meals/MealsSummary.js
@@ -3,14 +3,14 @@ import style from "./MealsSummary.module.css";
 export default function MealsSummary() {
   return (
     <section className={style.summary}>
-      <h2>Delicous Food, Delivered To You</h2>
+      <h2>Delicous Pizza, Delivered To You!</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
+        Choose your favorite pizza from our broad selection of available options
         and enjoy a delicious lunch or dinner at home.
       </p>
       <p>
-        All our meals are cooke dwith high-quality ingredients, just-in-time and
-        of course by experienced
+        All our pizza's are cooked with high-quality ingredients, just-in-time
+        and of course by experienced chefs!
       </p>
     </section>
   );
