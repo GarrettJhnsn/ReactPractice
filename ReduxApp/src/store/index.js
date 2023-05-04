93efab621987318c8afc5740c844827ed22d7154
Garrett Johnson <Garrett.Johnson@ey.com>
2023-05-04 12:30:31 -0700

    ReduxApp

--- ReduxApp/src/store/index.js
+++ ReduxApp/src/store/index.js
@@ -6,7 +6,6 @@ const counterReducer = (state = { counter: 0 }, action) => {
       counter: state.counter + 1,
     };
   }
-
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
@@ -15,7 +14,7 @@ const counterReducer = (state = { counter: 0 }, action) => {
   return state;
 };
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 const counterSubscriber = () => {
   const latestState = store.getState();
@@ -24,6 +23,5 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber);
 
-store.dispatch({ type: "increment" });
 store.dispatch({ type: "increment" });
 store.dispatch({ type: "decrement" });
