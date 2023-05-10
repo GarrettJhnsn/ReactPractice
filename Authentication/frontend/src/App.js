7975bc9756b6ac6ab36e8c33cf1ada8d2cc1b176
Garrett M Johnson <45772450+GarrettJhnsn@users.noreply.github.com>
2023-05-10 13:55:11 -0700

    JWT Auth

--- Authentication/frontend/src/App.js
+++ Authentication/frontend/src/App.js
@@ -13,16 +13,23 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
+import { tokenLoader, checkAuthLoader } from "./util/auth";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: "root",
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: 'events',
+        path: "events",
         element: <EventsRootLayout />,
         children: [
           {
@@ -31,8 +38,8 @@ const router = createBrowserRouter([
             loader: eventsLoader,
           },
           {
-            path: ':eventId',
-            id: 'event-detail',
+            path: ":eventId",
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
               {
@@ -41,23 +48,34 @@ const router = createBrowserRouter([
                 action: deleteEventAction,
               },
               {
-                path: 'edit',
+                path: "edit",
                 element: <EditEventPage />,
                 action: manipulateEventAction,
+                loadeR: checkAuthLoader,
               },
             ],
           },
           {
-            path: 'new',
+            path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: checkAuthLoader,
           },
         ],
       },
       {
-        path: 'newsletter',
+        path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
+      },
+      {
+        path: "auth",
+        element: <AuthenticationPage />,
+        action: authAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
     ],
   },
--- Authentication/frontend/src/components/MainNavigation.js
+++ Authentication/frontend/src/components/MainNavigation.js
@@ -1,9 +1,11 @@
-import { NavLink } from 'react-router-dom';
+import { Form, NavLink, useRouteLoaderData } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
+  const token = useRouteLoaderData("root");
+
   return (
     <header className={classes.header}>
       <nav>
@@ -38,6 +40,25 @@ function MainNavigation() {
             >
               Newsletter
             </NavLink>
+          </li>
+          <li>
+            {!token && (
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Login
+              </NavLink>
+            )}
+          </li>
+          <li>
+            {token && (
+              <Form action="/logout" method="post">
+                <button>Logout</button>
+              </Form>
+            )}
           </li>
         </ul>
       </nav>
